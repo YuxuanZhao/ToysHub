a8053f3e269d70c7e36fefa9da79267c69737c73
EC2 Default User <ec2-user@ip-172-31-58-172.ec2.internal>
2020-07-29 03:13:07 +0000

    mongoose toys database & show page

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var express = require("express"),
     app     = express(),
-    mongoose= require("mongoose");
+    mongoose= require("mongoose"),
+    bodyParser = require("body-parser");
     
 app.use(express.static("pages"));
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set("view engine", "ejs");
 mongoose.connect("mongodb://localhost/toys_hub", {useNewUrlParser: true, useUnifiedTopology: true});
 
@@ -14,32 +17,56 @@ var toySchema = new mongoose.Schema({
 
 var Toy = mongoose.model("Toy", toySchema);
 
-Toy.create({
-    name: "Freedom gundam",
-    picture: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.plazajapan.com%2F4573102580580%2F&psig=AOvVaw39djA-54VbORMDdo7YgV2H&ust=1596070259567000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCIihnJif8eoCFQAAAAAdAAAAABAI",
-    description: "The most popular gundam in China. Also, the Seed is very attractive in China's market."
-}, function(err, toy){
-    if (err){
-        console.log(err);
-    }
-    else{
-        console.log("Newly created toy:");
-        console.log(toy);
-    }
-});
+// Toy.create({
+//     name: "Freedom gundam",
+//     picture: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.plazajapan.com%2F4573102580580%2F&psig=AOvVaw39djA-54VbORMDdo7YgV2H&ust=1596070259567000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCIihnJif8eoCFQAAAAAdAAAAABAI",
+//     description: "The most popular gundam in China. Also, the Seed is very attractive in China's market."
+// }, function(err, toy){
+//     if (err){
+//         console.log(err);
+//     }
+//     else{
+//         console.log("Newly created toy:");
+//         console.log(toy);
+//     }
+// });
 
 app.get("/", function(req, res){
     res.render("../pages/landing");
 });
 
+app.get("/toys/new", function(req, res){
+    res.render("../pages/newToy");
+});
+
 app.get("/toys", function(req, res){
-    res.render("../pages/toys");
+
+    Toy.find({}, function(err, toys){
+      if (err){
+          console.log(err);
+      } else {
+          res.render("../pages/toys", {toys: toys});
+      } 
+    });
+    
 });
 
 app.post("/toys", function(req, res){
-    var newToy = req.body.newToy;
+    console.log(req.body);
+    var newToy = {
+        name: req.body.name,
+        picture: req.body.picture,
+        description: req.body.description
+    };
     
-})
+    Toy.create(newToy, function(err, newCreatedToy){
+        if (err){
+            console.log(err);
+        } else {
+            res.redirect("/toys");
+        }
+    });
+});
 
 app.get("*", function(req, res){
    res.send("Page not found!"); //work for all links that are not defined 
