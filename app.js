41e62102c4ad065b3e836093b13641acca4cf4ba
EC2 Default User <ec2-user@ip-172-31-58-172.ec2.internal>
2020-07-29 14:56:58 +0000

    add id page and bootstrap

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var express = require("express"),
     mongoose= require("mongoose"),
     bodyParser = require("body-parser");
     
-app.use(express.static("pages"));
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
@@ -17,28 +16,12 @@ var toySchema = new mongoose.Schema({
 
 var Toy = mongoose.model("Toy", toySchema);
 
-// Toy.create({
-//     name: "Freedom gundam",
-//     picture: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.plazajapan.com%2F4573102580580%2F&psig=AOvVaw39djA-54VbORMDdo7YgV2H&ust=1596070259567000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCIihnJif8eoCFQAAAAAdAAAAABAI",
-//     description: "The most popular gundam in China. Also, the Seed is very attractive in China's market."
-// }, function(err, toy){
-//     if (err){
-//         console.log(err);
-//     }
-//     else{
-//         console.log("Newly created toy:");
-//         console.log(toy);
-//     }
-// });
-
+//landing page
 app.get("/", function(req, res){
     res.render("landing");
 });
 
-app.get("/toys/new", function(req, res){
-    res.render("newToy");
-});
-
+//show all toys
 app.get("/toys", function(req, res){
 
     Toy.find({}, function(err, toys){
@@ -51,6 +34,12 @@ app.get("/toys", function(req, res){
     
 });
 
+//create a new toy form
+app.get("/toys/new", function(req, res){
+    res.render("newToy");
+});
+
+//create a new toy post
 app.post("/toys", function(req, res){
     console.log(req.body);
     var newToy = {
@@ -68,6 +57,20 @@ app.post("/toys", function(req, res){
     });
 });
 
+//show a specfic page
+app.get("/toys/:id", function(req, res){
+   Toy.findById(req.params.id).populate("comments").exec(function(err, foundToy){
+       if (err || !foundToy){
+           console.log(err);
+           return res.redirect("/toys");
+       } else {
+           console.log(foundToy);
+           res.render("show", {toy: foundToy});
+       }
+   }) 
+});
+
+//all other pages
 app.get("*", function(req, res){
    res.send("Page not found!"); //work for all links that are not defined 
 });
